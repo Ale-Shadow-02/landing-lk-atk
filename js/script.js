186013d810bece4ce5225c47811aa1dce76ae164
Ale-Shadow-02 <dvoryadkiin@yandex.ru>
2021-12-13 19:21:08 +0400

    Сверстана без адаптива меню сайта, в таботе мобильное меню

--- js/script.js
+++ js/script.js
@@ -10,8 +10,27 @@ document.addEventListener("DOMContentLoaded", function() {
       });
    };
 
-
    
+   let tabBtn = document.querySelectorAll('.tab__btn');
+   let tabsContent = document.querySelectorAll('.tabs__content');
+
+   if (menuBody) {
+      menuBody.addEventListener('click', (e) => {
+         if (e.target.classList.contains('tab__btn')) {
+            const tabsPath = e.target.dataset.tabsPath;
+            tabBtn.forEach(el => { el.classList.remove('tab__btn--active') });
+            document.querySelector(`[data-tabs-path="${tabsPath}"]`).classList.add('tab__btn--active');
+            tabsHandler(tabsPath);
+         }
+      })
+   }
+
+
+
+   const tabsHandler = (path) => {
+      tabsContent.forEach(el => { el.classList.remove('tabs__content--active') });
+      document.querySelector(`[data-tabs-target="${path}"]`).classList.add('tabs__content--active');
+   };
 });
 
 
