2ef0b9e2330b3912c897098c68daef5064fe68eb
Ale-Shadow-02 <dvoryadkiin@yandex.ru>
2021-12-14 11:34:36 +0400

    Сверстана и адаптировано мню сайта

--- js/script.js
+++ js/script.js
@@ -13,25 +13,30 @@ document.addEventListener("DOMContentLoaded", function() {
    
    let tabBtn = document.querySelectorAll('.tab__btn');
    let tabsContent = document.querySelectorAll('.tabs__content');
+   
 
    if (menuBody) {
       menuBody.addEventListener('click', (e) => {
          if (e.target.classList.contains('tab__btn')) {
             const tabsPath = e.target.dataset.tabsPath;
-            tabBtn.forEach(el => { el.classList.remove('tab__btn--active') });
-            document.querySelector(`[data-tabs-path="${tabsPath}"]`).classList.add('tab__btn--active');
+            tabBtn.forEach(el => { el.classList.remove('tab__btn--active');});
+            document.querySelector(`[data-tabs-path="${tabsPath}"]`).classList.add('tab__btn--active'); 
             tabsHandler(tabsPath);
          }
       })
    }
 
-
-
    const tabsHandler = (path) => {
       tabsContent.forEach(el => { el.classList.remove('tabs__content--active') });
       document.querySelector(`[data-tabs-target="${path}"]`).classList.add('tabs__content--active');
    };
+
+   
+   
 });
+
+
+
 
 
 
