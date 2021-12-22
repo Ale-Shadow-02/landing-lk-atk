e22618dc56a6143e7ff7e0d74958b7bb8ac12307
Ale-Shadow-02 <dvoryadkiin@yandex.ru>
2021-12-22 19:48:47 +0400

    Сверстано с адаптивом секция формы и модальное окно

--- js/script.js
+++ js/script.js
@@ -33,9 +33,14 @@ document.addEventListener("DOMContentLoaded", function() {
    // Slider-1
    let slider_1 = new Swiper('.slider_1', {
       pagination: {
-         el: '.swiper-pagination',
+         el: '.my1__pagin',
          clickable: true,
       },
+      navigation: {
+         nextEl: ".my1-button-next",
+         prevEl: ".my1-button-prev",
+      },
+      grabCursor: true,
    });
    //Slider-2
    let slider_2 = new Swiper(".mySwiper", {
@@ -80,6 +85,98 @@ document.addEventListener("DOMContentLoaded", function() {
       grabCursor: true,
    });
    
+   
+   // Modal Open
+   const popupLinks = document.querySelectorAll('.popup-link');
+   const body = document.querySelector('body');
+   const lockPadding = document.querySelectorAll(".lock-padding");
+
+   let unlock = true;
+   const timeout = 800;
+
+   if (popupLinks.length > 0) {
+      for (let index = 0; index < popupLinks.length; index++) {
+         const popupLink = popupLinks[index];
+         popupLink.addEventListener("click", function (e) {
+            const popupName = popupLink.getAttribute('href').replace('#', '');
+            const curentPopup = document.getElementById(popupName);
+            popupOpen(curentPopup);
+            e.preventDefault();
+         });
+      }
+   }
+   const popupCloseIcon = document.querySelectorAll('.close-popup');
+   if (popupCloseIcon.length > 0) {
+      for (let index = 0; index < popupCloseIcon.length; index++) {
+         const el = popupCloseIcon[index];
+         el.addEventListener('click', function (e) {
+            popupClose(el.closest('.popup'));
+            e.preventDefault();
+         });
+      }
+   }
+
+   function popupOpen(curentPopup) {
+      if (curentPopup && unlock) {
+         const popupActive = document.querySelector('.popup.open');
+         if (popupActive) {
+            popupClose(popupActive, false);
+         } else {
+            bodyLock();
+         }
+         curentPopup.classList.add('open');
+         curentPopup.addEventListener("click", function (e) {
+            if (!e.target.closest('.popup__content')) {
+               popupClose(e.target.closest('.popup'));
+            }
+         });
+      }
+   }
+
+   function popupClose(popupActive, doUnlock = true) {
+      if (unlock) {
+         popupActive.classList.remove('open');
+         if (doUnlock) {
+            bodyUnLock();
+         }
+      }
+   }
+
+   function bodyLock() {
+      const lockPaddingValue = window.innerWidth - document.querySelector('.wrapper').offsetWidth + 'px';
+
+      if (lockPadding.length > 0) {
+         for (let index = 0; index < lockPadding.length; index++) {
+            const el = lockPadding[index];
+            el.style.paddingRight = lockPaddingValue;
+         }
+      }
+      body.style.paddingRight = lockPaddingValue;
+      body.classList.add('lock');
+
+      unlock = false;
+      setTimeout(function () {
+         unlock = true;
+      }, timeout);
+   }
+
+   function bodyUnLock() {
+      setTimeout(function () {
+         if (lockPadding.length > 0) {
+            for (let index = 0; index < lockPadding.length; index++) {
+               const el = lockPadding[index];
+               el.style.paddingRight = '0px';
+            }
+         }
+         body.style.paddingRight = '0px';
+         body.classList.remove('lock');
+      }, timeout);
+
+      unlock = false;
+      setTimeout(function () {
+         unlock = true;
+      }, timeout);
+   }
 });
 
 
